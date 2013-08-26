28a51d21983f0699bf60672cbbfc0a6b98c68491
Bradley Geddes <bradtg@gmail.com>
2013-08-25 18:17:29 -0700

    Add User Model with Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Omrails::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+ #In production, :host should be set to the actual host of your application.
+ config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Omrails::Application.routes.draw do
 
+  devise_for :users
+
   root :to => 'pages#home'
   
   get 'about' => 'pages#about'
